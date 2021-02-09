7e9e31dc726dc4e59c185f0613cf8cfa2ca13a0f
Simone Cardinale <simonecardinale8@gmail.com>
2021-02-09 19:57:21 +0100

    work complete + bonus

--- main.js
+++ main.js
@@ -1,54 +1,67 @@
 var app = new Vue({
 	el: "#app",
 	data: {
-		allMusic:[],
-		rockArray:[],
-		popArray:[],
-		jazzArray:[],
-		metalArray:[],
-		genreArray:["All"],
+		arrayObj: {
+			All:[],
+			Rock:[],
+			Pop:[],
+			Jazz:[],
+			Metal:[],
+		},
+		genreArray:[],
 		showArray:[],
-		genereSelezionato: "All",
 	},
 	mounted() {
 		axios.get('https://flynn.boolean.careers/exercises/api/array/music').then(result => {
-			this.allMusic = result.data.response
+			this.arrayObj.All = result.data.response
 			for(let k in result.data.response) {
-				if(this.genreArray.includes(result.data.response[k].genre)== false){
-					this.genreArray.push(result.data.response[k].genre)
-				} 
 				if(result.data.response[k].genre == "Rock"){
-					this.rockArray.push(result.data.response[k])
+					this.arrayObj.Rock.push(result.data.response[k])
 				} else if(result.data.response[k].genre == "Pop"){	
-					this.popArray.push(result.data.response[k])
+					this.arrayObj.Pop.push(result.data.response[k])
 				} else if(result.data.response[k].genre == "Jazz"){	
-					this.jazzArray.push(result.data.response[k])
+					this.arrayObj.Jazz.push(result.data.response[k])
 				} else if(result.data.response[k].genre == "Metal"){	
-					this.metalArray.push(result.data.response[k])
+					this.arrayObj.Metal.push(result.data.response[k])
+				}
+				for(k in this.arrayObj) {
+					if(this.genreArray.includes(k)== false){
+
+						this.genreArray.push(k)
+						console.log(this.genreArray);
+					}
 				}
 			}
-			this.genereSelezionato = "All"
-			console.log(this.genereSelezionato);
+			this.showArray = this.arrayObj.All
 		});
 
 	},
 
 	methods: {
 		changeGenre(index){
-			this.genereSelezionato = this.genreArray[index]
-			console.log(this.genereSelezionato);
-			if(this.genereSelezionato == "All"){
-				this.showArray = this.allMusic
-				console.log(this.allMusic);
-			} else if(this.genereSelezionato == "Rock") {
-				this.showArray = this.rockArray
-			} else if(this.genereSelezionato == "Jazz") {
-				this.showArray = this.jazzArray
-			} else if(this.genereSelezionato == "Pop") {
-				this.showArray = this.popArray
-			} else if(this.genereSelezionato == "Metal") {
-				this.showArray = this.metalArray
+			// if(this.genereSelezionato == "All"){
+			// 	this.showArray = this.allMusic
+			// 	console.log(this.allMusic);
+			// } else if(this.genereSelezionato == "Rock") {
+			// 	this.showArray = this.rockArray
+			// } else if(this.genereSelezionato == "Jazz") {
+			// 	this.showArray = this.jazzArray
+			// } else if(this.genereSelezionato == "Pop") {
+			// 	this.showArray = this.popArray
+			// } else if(this.genereSelezionato == "Metal") {
+			// 	this.showArray = this.metalArray
+			// }
+
+			for(k in this.arrayObj) {
+				if(this.genreArray[index] == k) {
+					this.showArray = (this.arrayObj[k])
+					console.log(this.showArray);
+				}
 			}
 		}
 	},
 });
+
+
+
+
